d6befb987e2c4665c7d701e922e24f2a69ee3a26
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2017-02-01 06:48:01 +0100

    Show read text
    Rendering the annotated text will be triggered by updating the
    position in the app state, based on some timer. The
    requestAnimationFrame function in the browser could be a good
    candidate for this. But first displaying it correctly is more
    important.

--- src/read_by_hearing/core.cljs
+++ src/read_by_hearing/core.cljs
@@ -1,23 +1,78 @@
 (ns read-by-hearing.core
-  (:require [om.core :as om :include-macros true]
+  (:require [clojure.string :as str]
+            [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text-grid [{:word "Irgendwo" :end-time 0.44}
+                                      {:word "im" :end-time 0.57}
+                                      {:word "Mittelmeer" :end-time 1.05}
+                                      {:word "liegt" :end-time 1.33}
+                                      {:word "eine" :end-time 1.52}
+                                      {:word "Insel" :end-time 1.9886836799130783}
+                                      {:word "von" :end-time 2.54}
+                                      {:word "der" :end-time 2.64}
+                                      {:word "viele" :end-time 2.98}
+                                      {:word "Menschen" :end-time 3.3}
+                                      {:word "glauben," :end-time 3.68}
+                                      {:word "dass" :end-time 3.82}
+                                      {:word "sie" :end-time 3.93}
+                                      {:word "verzaubert" :end-time 4.48}
+                                      {:word "sein" :end-time 4.79}
+                                      {:word "könnte." :end-time 5.166705795701746}]
+                          :position 0}))
+
+(defn read-words
+  "Returns a string of all words annotated with an end time less or equal than
+   position. These are the words, that have been read already."
+  [text-grid position]
+  (->> text-grid
+       (filter #(<= (:end-time %) position))
+       (map :word)
+       (str/join " ")
+       (not-empty)))
+
+(defn unread-words
+  "Returns a string of all words annotated with an end time greater than
+   position. These are the words, that have not been read until position."
+  [text-grid position]
+  (->> text-grid
+       (filter #(> (:end-time %) position))
+       (map :word)
+       (str/join " ")
+       (not-empty)))
+
+(defn ensure-separated-unread
+  "Returns unread with a leading \" \" when both exist. Thus they can be put
+   together into the HTML document.
+
+   Returns unread otherwise."
+  [read unread]
+  (if (and read unread)
+    (str " " unread)
+    unread))
+
+(defn render-text-grid
+  "Returns a component containing the currently read and unread words from the
+   given text grid. The given position is used as current."
+  [{:keys [text-grid position]}]
+  (om/component
+   (dom/p nil
+          (let [read (read-words text-grid position)
+                unread (unread-words text-grid position)]
+            (dom/p nil
+                   (dom/span #js {:className "read"} read)
+                   (ensure-separated-unread read unread))))))
+
 
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/div nil
-                 (dom/h1 nil (:text data))
-                 (dom/h3 nil "Edit this and watch it change!")))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+ (fn [data owner]
+   (render-text-grid data))
+ app-state
+ {:target (. js/document (getElementById "app"))})
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn update-position!
+  "Sets the position in the app state to the given value."
+  [new-position]
+  (swap! app-state #(assoc % :position new-position)))
